abb28615170cbd1210e32c0a0b23c4f134b7fd3f
Christopher Hall <hsw@bitmark.com>
2017-01-26 14:32:21 +0800

    add a version number
    Signed-off-by: Christopher Hall <hsw@bitmark.com>

---
+++ version.go
@@ -0,0 +1,12 @@
+// Copyright (c) 2014-2017 Bitmark Inc.
+// Use of this source code is governed by an ISC
+// license that can be found in the LICENSE file.
+
+package exitwithstatus
+
+// ensure that git has a tag: "vX.Y" corresponding to major and minor
+const (
+	Major   = "1"
+	Minor   = "1"
+	Version = Major + "." + Minor
+)
