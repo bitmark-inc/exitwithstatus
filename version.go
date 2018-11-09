74f40d6762dfebaabf7997f9623a672d98630676
Christopher Hall <hsw@bitmark.com>
2018-11-09 16:01:45 +0800

    Convert to module
    Note that the old two number versions are considered as 0.y.z
    for conversion to three number versions
    
    Signed-off-by: Christopher Hall <hsw@bitmark.com>

--- version.go
+++ version.go
@@ -1,12 +1,13 @@
-// Copyright (c) 2014-2017 Bitmark Inc.
+// Copyright (c) 2014-2018 Bitmark Inc.
 // Use of this source code is governed by an ISC
 // license that can be found in the LICENSE file.
 
 package exitwithstatus
 
-// ensure that git has a tag: "vX.Y" corresponding to major and minor
+// ensure that git has a tag: "vX.Y.Z" corresponding to major, minor and patch
 const (
-	Major   = "1"
+	Major   = "0"
 	Minor   = "1"
-	Version = Major + "." + Minor
+	Patch   = "2"
+	Version = Major + "." + Minor + "." + Patch
 )
